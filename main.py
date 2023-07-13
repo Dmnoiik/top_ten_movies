c1f92d1012759dc578f79cc3140267303009ec8d
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2023-07-13 13:15:48 +0200

    Working on editing the movie after adding it

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from forms import EditForm, AddForm
+from credentials import movies_api, movies_read_access
 import requests
 
 app = Flask(__name__)
@@ -17,9 +18,9 @@ class Movie(db.Model):
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
     img_url = db.Column(db.String(250), nullable=False)
 
 
@@ -37,7 +38,6 @@ def edit_movie():
     if request.method == 'POST':
         new_rating = float(request.form['rating_field'])
         new_review = request.form['review_field']
-        print(new_rating)
         with app.app_context():
             current_movie = db.get_or_404(Movie, request.args['id'])
             current_movie.rating = new_rating
@@ -62,10 +62,40 @@ def add_movie():
     add_form = AddForm()
     if request.method == "POST":
         movie_title = request.form['title']
-        response = requests.get('')
-        return render_template('select.html')
+        url = "https://api.themoviedb.org/3/search/movie"
+        params = {
+            "query": movie_title
+        }
+        headers = {
+            "accept": "application/json",
+            "Authorization": F"Bearer {movies_read_access}"
+        }
+        response = requests.get(url, params=params, headers=headers).json()['results']
+        return render_template('select.html', movies=response)
     if request.method == "GET":
-        return render_template('add.html', form=add_form)
+        if len(request.args) == 1:
+            url = F"https://api.themoviedb.org/3/movie/{request.args['id']}"
+            headers = {
+                "accept": "application/json",
+                "Authorization": F"Bearer {movies_read_access}"
+            }
+            response = requests.get(url, headers=headers).json()
+            with app.app_context():
+                new_movie = Movie(
+                    title=response['original_title'],
+                    description=response['overview'],
+                    year=int(response['release_date'].split('-')[0]),
+                    img_url='https://image.tmdb.org/t/p/w500' + response['poster_path'],
+                    rating=None,
+                    review=None,
+                    ranking=None
+
+                )
+                db.session.add(new_movie)
+                db.session.commit()
+            return redirect('/')
+        else:
+            return render_template('add.html', form=add_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
